f8730b9a9e00f889daa42edc398b5480849365a9
AnnaXu <anna.xu@sap.com>
2018-10-04 13:21:07 +0800

    submit1

--- src/main/java/com/pluralsight/ShoppingCart.java
+++ src/main/java/com/pluralsight/ShoppingCart.java
@@ -42,6 +42,15 @@ public class ShoppingCart {
  public void setOrderTotal(double dblOrderTotal) {
   this.dblOrderTotal = dblOrderTotal;
  }
+ 
+ public void deleteCartItem(int index) {
+	 try {
+		 cartItems.remove(index);
+	 }catch(IndexOutOfBoundsException e) {
+		 e.printStackTrace();
+	 }
+	 
+ }
 
  protected void calculateOrderTotal() {
   double dblTotal = 0;
